bde7fab6c296c4f46c381318f0df47b8c5f1ec75
khushitapanchal <khushita1510@gmail.com>
2026-02-23 12:43:24 +0530

    Backend ready for deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,12 @@
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # change later to frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 from fastapi import FastAPI
 from .routes import auth, customers, services, appointments, dashboard, users
 from .database import engine, Base
