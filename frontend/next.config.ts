e55f2313776de70ca5f8f743ec22dce7b3bd7de1
khushitapanchal <khushita1510@gmail.com>
2026-02-25 15:32:21 +0530

    update next config for railway

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: "standalone",
 };
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
 
 export default nextConfig;
